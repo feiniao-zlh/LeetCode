d3df07edd237a276cc4004dc0694ce25cafc23cb
FeiNiao <742738858@qq.com>
2023-10-15 17:20:41 +0800

    test

---
+++ algorithm/Dfs_Bfs.java
@@ -0,0 +1,27 @@
+package algorithm;
+
+// 数字全排列
+
+public class Dfs_Bfs {
+    public static void main(String[] args) {
+        //int step=2;
+        int n = 10;
+        int[] num = {1};
+
+        // 循环读取可使用的数字
+        for (int i = 1; i <= n; i++) {
+            if (num[i]==1){
+                // 放入盒子进行标记
+                num[i]=0;
+                // 进行下一个盒子判断检查
+
+            }
+        }
+
+    }
+
+    void dfs(int step) {
+
+    }
+
+}
